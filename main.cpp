f93dc2c457fd7deeed420942ac9b361f6bf8d788
Nicowong <nicolas.wongwanit@gmail.com>
2016-12-08 11:05:17 +0100

    main

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "generalFunctions.hpp"
+#include "Point.hpp"
+#include "Rectangle.hpp"
+#include "Object.hpp"
+#include "Item.hpp"
+#include "Bullet.hpp"
+#include "Unit.hpp"
+#include "Player.hpp"
+#include "Enemy.hpp"
+#include "Explosion.hpp"
+#include "Decor.hpp"
+
+using namespace std ;
+
+int main(int argv, char** argc){
+	return 0 ;
+}
