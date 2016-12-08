e66718cc6a5469f1270b2461219c7eb510f9ab2a
Nicowong <nicolas.wongwanit@gmail.com>
2016-12-08 11:49:03 +0100

    weapon definition

---
+++ WEAPON_CONFIG.hpp
@@ -0,0 +1,18 @@
+#ifndef WEAPON_CONFIG_HPP
+#define WEAPON_CONFIG_HPP
+
+#define SHOVEL_DMG	1
+#define VODKA_DMG	4
+#define GUN_DMG		2
+#define LASER_DMG	12
+#define MISSILE_DMG	8
+#define NUKE_DMG	50
+
+#define SHOVEL_PIC
+#define VODKA_PIC	
+#define GUN_PIC		
+#define LASER_PIC	
+#define MISSILE_PIC	
+#define NUKE_PIC	
+
+#endif
--- main.cpp
+++ main.cpp
@@ -13,5 +13,10 @@
 using namespace std ;
 
 int main(int argv, char** argc){
+
+	const Weapon wpn_def[6] = {	Weapon("wpn_shovel", SHOVEL_DMG)	, Weapon("wpn_vodka", VODKA_DMG), 
+								Weapon("wpn_gun", GUN_DMG)			, Weapon("wpn_laser", LASER_DMG),
+								Weapon("wpn_missile", MISSILE_DMG)	, Weapon("wpn_nuke", NUKE_DMG)	 };
+
 	return 0 ;
 }
