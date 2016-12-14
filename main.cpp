2b6e8dfa0dc0f4a5500f88019b2af748e360a2a0
IlyaBrod <ilya.brodoline@ensea.fr>
2016-12-14 18:24:31 +0100

    Il y a une erreur dans la definition des fonctions de Point +=

--- main.cpp
+++ main.cpp
@@ -14,10 +14,13 @@
 using namespace std ;
 
 int main(int argv, char** argc){
-
+	
+	/*
 	const Weapon wpn_def[6] = {	Weapon(SHOVEL_PIC, SHOVEL_DMG)	, Weapon(VODKA_PIC, VODKA_DMG), 
 								Weapon(GUN_PIC, GUN_DMG)		, Weapon(LASER_PIC, LASER_DMG),
 								Weapon(MISSILE_PIC, MISSILE_DMG), Weapon(NUKE_PIC, NUKE_DMG)	};
-
+	
+	*/
+	
 	return 0 ;
 }
