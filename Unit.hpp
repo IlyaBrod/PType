b71d95e6721f9c5d9db74a616dd3c565cb8ea3e2
IlyaBrod <ilya.brodoline@ensea.fr>
2016-12-15 08:29:11 +0100

    Reparation de bugs de la classe Point, Unit. Il reste a modifier Weapon

--- Unit.hpp
+++ Unit.hpp
@@ -2,12 +2,16 @@
 #define ClASS_OBJECT_HPP
 
 #include "Object.hpp"
+#include "Weapon.hpp"
 
 class Unit : public Object{
 	protected :
 		int life ;
+		Weapon weapon;
+		Weapon* weapons[];
+		
 	public :
-		Unit(sf::Texture texture, Point point, int life);
+		Unit(sf::Texture texture, Point point, Rectangle box, int life);
 		
 		void inCollide();
 
