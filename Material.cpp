047b631d756cb8e11284638b6d5e96fc264aee9a
IlyaBrod <ilya.brodoline@ensea.fr>
2016-12-19 19:09:24 +0100

    Material hérite de Sprite. On peut utiliser les méthodes de transformation et resize

--- Material.cpp
+++ Material.cpp
@@ -6,8 +6,8 @@ Material::Material()
 	texture -> create(10, 10);
 	sf::Uint8* pixels = new sf::Uint8[10 * 10 * 4];
 	texture -> update(pixels);
-	sprite = sf::Sprite();
-	sprite.setTexture(*texture);
+	
+	this -> setTexture(*texture);
 	
 	
 }
@@ -25,35 +25,15 @@ Material::Material(std::string path,bool smooth)
 	
 	if(smooth==true) texture->setSmooth(true);
 	
-	sprite = sf::Sprite();
-	sprite.setTexture(*texture);
+	this -> setTexture(*texture);
 	
 }
 
 
 void Material::draw(sf::RenderWindow& window)
 {
-	window.draw(sprite);
+	window.draw(*this);
 	
 }
 
-/*sf::Sprite* newSprite(std::string path)
-{
-	sf::Texture texture ;
-
-	if (!texture.loadFromFile(path))
-	{
-		texture.create(10, 10);
-		sf::Uint8* pixels = new sf::Uint8[10 * 10 * 4];
-		texture.update(pixels);
-	}
-	texture.setSmooth(true);
-
-	sf::Sprite *sprite = new sf::Sprite();
-	sprite->setTexture(texture);
-
-	return sprite;
-}
-
-*/
 
--- Material.hpp
+++ Material.hpp
@@ -4,11 +4,15 @@
 
 #include <SFML/Graphics.hpp>
 //#include <iostream>
-class Material
+
+class Material : public sf::Sprite
 {
 	private:
 		sf::Texture* texture;
-		sf::Sprite sprite;
+		
+		int x;
+		int y;
+		
 	public:
 		Material();
 		Material(std::string path,bool smooth=true);
@@ -19,6 +23,40 @@ class Material
 		 */
 		void draw(sf::RenderWindow& window);
 	
+	
+		/**
+		 * Move the material by increasing the location by x
+		 * @param x
+		 */
+		 void moveX(int x);
+		 
+		 
+		/**
+		 * Move the material by increasing the location by y
+		 * @param y
+		 */
+		 void moveY(int y);
+		 
+		 
+		/**
+		 * Move the material by increasing the location by x and y
+		 * @param x
+		 * @param y
+		 */
+		 void move(int x=0,int y=0);
+		 
+		 /**
+		  * Set the X coordinate to a value
+		  * @param x
+		  */
+		 void setX(int x);
+		 
+		 
+		 /**
+		  * Set the Y coordinate to a value
+		  * @param y
+		  */
+		 void setY(int y);
 };
 
 
