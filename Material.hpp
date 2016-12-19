7db316924d9eba0feff78f581e22c9262f646f99
IlyaBrod <ilya.brodoline@ensea.fr>
2016-12-19 11:37:03 +0100

    Add Material Files

---
+++ Material.hpp
@@ -0,0 +1,34 @@
+
+#ifndef CLASS_MATERIAL_HPP
+#define CLASS_MATERIAL_HPP
+
+class Material
+{
+	
+	
+	
+};
+
+
+#endif
+
+/*sf::Sprite* newSprite(std::string path)
+{
+	sf::Texture texture ;
+
+	if (!texture.loadFromFile(path))
+	{
+		texture.create(10, 10);
+		sf::Uint8* pixels = new sf::Uint8[10 * 10 * 4];
+		texture.update(pixels);
+	}
+	texture.setSmooth(true);
+
+	sf::Sprite *sprite = new sf::Sprite();
+	sprite->setTexture(texture);
+
+	return sprite;
+}
+
+*/
+
