830919ec9b3cc183c430d3189578df2806cd2e76
IlyaBrod <ilya.brodoline@ensea.fr>
2017-01-05 09:06:49 +0100

    class Button v0.2

--- Interface/Button.cpp
+++ Interface/Button.cpp
@@ -1,4 +1,34 @@
 #include "Button.hpp"
 
 
+Button::Button()
+{
+	mouseRec = Rectangle();
+	pressed=false;
+	mouseRec.setH(5);
+	mouseRec.setW(5);
+}
 
+Button::Button(int x, int y, int width, int height)
+{
+	setH(height);
+	setW(width);
+	setX(x);
+	setY(y);
+	mouseRec.setH(5);
+	mouseRec.setW(5);
+	pressed=false;
+}
+
+void Button::refresh()
+{
+	sf::Vector2i vec = getPosition();
+	
+	mouseRec.setX(vec.x);
+	mouseRec.setY(vec.y);
+	
+	if(intersect(mouseRec)==true)
+	{
+		event=true;
+	}
+}
--- Interface/Button.hpp
+++ Interface/Button.hpp
@@ -2,8 +2,27 @@
 #ifndef CLASS_BUTTON_HPP
 #define CLASS_BUTTON_HPP
 
-class Button
+#include "../Basic/Rectangle.hpp"
+#include "../Basic/Material"
+#include <SFML/Graphics.hpp>
+
+class Button : public Material, public sf::Mouse, public Rectangle
 {
+	private:
+	Rectangle mouseRec;
+	bool pressed;
+	
+	public:
+	
+	Button();
+	Button(int x,int y, int width, int height);
+	
+	/**
+	 * Verify if the button is pressed
+	 */
+	void refresh();
+	
+	
 	
 	
 	
