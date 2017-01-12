f3f529eed04b4a745d9505242e9699af2ca1926d
Nicowong <nicolas.wongwanit@gmail.com>
2017-01-12 10:16:46 +0100

    object move

--- Game/Object.hpp
+++ Game/Object.hpp
@@ -42,6 +42,7 @@ class Object : public Point, public Material{
         void set_alive(const bool alive){this->alive = alive ;}
         void set_visible(bool visible){this->visible = visible;}
 
+        void move(int x, int y);
     //implement collision effects (virtual function)
         virtual void inCollide(Object* obj)=0;
 		/**
