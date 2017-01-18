63deb0e60531548226948f79c3d85cedcd1b4cb5
IlyaBrod <ilya.brodoline@ensea.fr>
2017-01-18 19:41:37 +0100

    add pics

--- Config/ENEMY_CONFIG.hpp
+++ Config/ENEMY_CONFIG.hpp
@@ -3,5 +3,6 @@
 
 #define ENEMY_BEAR          "Images/Enemy/bear.png"
 #define ENEMY_POLAR_BEAR    "Images/Enemy/polarBear.png"
+#define BOX					"Images/Enemy/box.png"
 
 #endif // ENEMY_CONFIG_HPP_INCLUDED
