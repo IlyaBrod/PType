813bb2acb76340dd703843581db465f8fa595945
Nicowong <nicolas.wongwanit@gmail.com>
2017-01-18 13:44:30 +0100

    autoMove enemy
    automove enemy

--- Game/Enemy.cpp
+++ Game/Enemy.cpp
@@ -2,7 +2,7 @@
 
 Enemy::Enemy(std::string texturePath, Point point, sf::IntRect box, int life, int collideDmg):
     Unit(texturePath, point, box, life, collideDmg){
-
+    speed = ENEMY_DEFAULTSPEED ;
 }
 
 void Enemy::inCollide(Object* obj){
@@ -18,6 +18,29 @@ void Enemy::inCollide(Object* obj){
         case DECOR :
             break ;
         default :
+            break ;
+    }
+}
+
+void Enemy::autoMove(int t, Object *obj){
+    switch(t){
+        case STRAIGHT :
+            move(-speed, 0);
+            break ;
+        case HOMING :
+            if(obj != NULL){
+                int dx=obj->getX()-getX(), dy = obj->getY()-getY();
+                move(dx/(dx+dy)*speed, dy/(dx+dy)*speed);
+            }else{
+                move(-speed, 0);
+            }
+            break ;
+        case HOMING_STRAIGHT :
+            if(obj != NULL){
+                int dx=obj->getX()-getX(), dy = obj->getY()-getY();
+                move(dx/(dx+dy)*speed/2, dy/(dx+dy)*speed/2);
+            }
+            move(-speed*2/3, 0);
             break ;
     }
 }
